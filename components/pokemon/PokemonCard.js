66775e03fe401f586081e998b41a070d07a656f3
Davide <ceredavide@live.it>
2020-04-22 01:21:56 +0200

    reso conmponente una funzione

--- components/pokemon/PokemonCard.js
+++ components/pokemon/PokemonCard.js
@@ -2,32 +2,49 @@ import React from "react";
 
 import { StyleSheet, View, Text } from "react-native";
 
-export default class PokemonCard extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            loading: false,
-        }
-    }
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp
+} from "react-native-responsive-screen";
 
+import Types from "./Types"
 
-
-  render() {
-    const { pokemon } = this.props;
-
-    return (
-      <View>
-        <Text>weight: {pokemon.weight / 10} kg</Text>
-        <Text>height: {pokemon.height / 10} meters</Text>
-        <Text>
-          Types:
-          {pokemon.types.map(item => {
-            return <Text key={item.slot}>{item.type.name}</Text>;
-          })}
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <View style={styles.card}>
+      <Types types={pokemon.types} />
+      <Text>weight: {pokemon.weight / 10} kg</Text>
+      <Text>height: {pokemon.height / 10} meters</Text>
+      <Text>base_experience: {pokemon.base_experience}</Text>
+      <Text>abilities: {pokemon.abilities.map((item, index) => {
+        return (
+          <Text key={index}>{item.ability.name} </Text>
+        )
+      })}</Text>
+      <Text>
+        Stats:
+         {pokemon.stats.map((item, index) => {
+        return <Text key={index}>
+          {item.stat.name}: {item.base_stat}
         </Text>
-      </View>
-    );
-  }
+      })}
+      </Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  card: {
+    marginTop: -hp("2%"),
+    marginBottom: hp("3%"),
+    padding: hp("2%"),
+    borderRadius: 40,
+    width: wp("100%"),
+    height: hp("60%"),
+    alignSelf: "center",
+    backgroundColor: "white"
+  }
+});
+
+export default PokemonCard
