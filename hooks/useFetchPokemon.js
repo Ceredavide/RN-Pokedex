17fe3299ad9c775f2c79a8521621cad7c326aeee
Davide <ceredavide@live.it>
2020-10-09 15:16:55 +0200

    fetcho piu di un url

--- hooks/useFetchPokemon.js
+++ hooks/useFetchPokemon.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 import handleError from "../services/handleError"
 
-const useFetchPokemon = (index) => {
+const useFetchPokemon = (url) => {
     const [isLoading, setIsloading] = useState(false)
     const [pokemon, setPokemon] = useState(null)
 
@@ -11,8 +11,15 @@ const useFetchPokemon = (index) => {
         const fetchPokemon = async () => {
             setIsloading(true)
             try {
-                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${index}`)
-                setPokemon(response.data)
+                
+                const response1 = await axios.get(url)
+
+                const response2 = await axios.get(response1.data.varieties[0].pokemon.url)
+
+                // metto in ordine i tipi per i pokemon
+                response2.data.types.sort((a, b) => { return a.slot - b.slot })
+
+                setPokemon({ ...response1.data, ...response2.data })
             } catch (error) {
                 handleError(error)
             } finally {
