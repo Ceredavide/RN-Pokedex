2fcb76f96d5547b3c6c7146e5be5ace280331240
Davide <ceredavide@live.it>
2020-10-09 15:16:24 +0200

    implementato styled

--- screens/Pokedex.js
+++ screens/Pokedex.js
@@ -1,73 +1,82 @@
 import React from "react";
-import {
-    StyleSheet,
-    SafeAreaView,
-    View,
-    Text,
-    ActivityIndicator,
-    FlatList,
-} from "react-native";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 
 import {
     heightPercentageToDP as hp,
     widthPercentageToDP as wp
 } from "react-native-responsive-screen";
 
+import styled from "styled-components/native"
+
+import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from "@expo/vector-icons";
 
 import ButtonPokemon from "../components/pokedex/ButtonPokemon";
 
-import useFetchPokedex from "../hooks/useFetchPokedex"
+import pokedexList from "../constants/pokedexList"
 
 const PokedexScreen = ({ navigation }) => {
 
-    const { isLoading, pokedex } = useFetchPokedex(0, 20)
+    let pokedex = pokedexList["gen4"]
 
     const renderItem = ({ item, index }) => (
         <ButtonPokemon
-            name={item.name}
+            name={item.pokemon_species.name}
             index={index}
+            url={item.pokemon_species.url}
             navigation={navigation}
         />
     )
 
     return (
         <SafeAreaView>
-            <View style={styles.header}>
+            <LinearGradient
+                start={[0.1, 0.1]}
+                colors={["#F0CB35", "#F66F71"]}
+                style={styles.gradient}
+            />
+            <Header>
                 <Ionicons
                     name="ios-arrow-round-back"
                     size={hp("5%")}
-                    color="black"
+                    color="#FFF"
                     onPress={() => navigation.goBack()}
                 />
-                <Text style={styles.title}>Pokedex</Text>
-            </View>
-            {isLoading ? (
-                <ActivityIndicator style={{ marginBottom: hp("50%") }} />
-            ) : (
-                    <FlatList
-                        data={pokedex}
-                        numColumns={2}
-                        columnWrapperStyle={styles.flatList}
-                        renderItem={renderItem}
-                        keyExtractor={item => item.url}
-                    />
-                )}
+                <Title>Pokedex</Title>
+            </Header>
+            <FlatList
+                data={pokedex.pokemon_entries}
+                numColumns={2}
+                columnWrapperStyle={styles.flatList}
+                renderItem={renderItem}
+                keyExtractor={item => item.entry_number}
+            />
         </SafeAreaView>
     );
 }
 
+const Header = styled.View`
+    padding: ${hp("2%")}px
+`;
+
+const Title = styled.Text`
+    margin-top: ${hp("1%")}px;
+    font-size: ${hp("5%")}px;
+    font-family: "Avenir-Heavy";
+    color: #FFF
+`;
 
 const styles = StyleSheet.create({
-    header: { padding: hp("2%") },
-    title: {
-        marginTop: hp("1%"),
-        fontSize: hp("4%"),
-        fontFamily: "Avenir-Heavy"
-    },
     flatList: {
         justifyContent: "space-between",
         marginHorizontal: wp("3%"),
+    },
+    gradient: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: hp("100%"),
     }
 });
 
