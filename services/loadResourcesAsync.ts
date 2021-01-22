715671aed362e974bc334f0487ed82cfe330a0d7
Davide <ceredavide@live.it>
2021-01-22 10:51:29 +0100

    rimosso require icone svg

--- services/loadResourcesAsync.ts
+++ services/loadResourcesAsync.ts
@@ -4,27 +4,9 @@ import * as Font from "expo-font";
 export default async function loadResourcesAsync() {
   try {
     await Asset.loadAsync([
-      require("../assets/icons/types/bug.svg"),
-      require("../assets/icons/types/dark.svg"),
-      require("../assets/icons/types/dragon.svg"),
-      require("../assets/icons/types/electric.svg"),
-      require("../assets/icons/types/fairy.svg"),
-      require("../assets/icons/types/fighting.svg"),
-      require("../assets/icons/types/fire.svg"),
-      require("../assets/icons/types/flying.svg"),
-      require("../assets/icons/types/ghost.svg"),
-      require("../assets/icons/types/grass.svg"),
-      require("../assets/icons/types/ground.svg"),
-      require("../assets/icons/types/ice.svg"),
-      require("../assets/icons/types/normal.svg"),
-      require("../assets/icons/types/poison.svg"),
-      require("../assets/icons/types/psychic.svg"),
-      require("../assets/icons/types/rock.svg"),
-      require("../assets/icons/types/steel.svg"),
-      require("../assets/icons/types/water.svg"),
       require("../assets/images/loadingPokemon.gif"),
-    ]),
-      await Font.loadAsync({
+    ])
+    await Font.loadAsync({
         "Avenir-Book": require("../assets/fonts/Avenir-Book.otf"),
         "Avenir-Heavy": require("../assets/fonts/Avenir-Heavy.ttf"),
         "Avenir-Medium": require("../assets/fonts/Avenir-Medium.otf"),
