f4cc9f70b8dc09254dfa27a50d125f5e42f8e755
Davide <ceredavide@live.it>
2021-01-28 20:32:40 +0100

    aggiunto safeareaprovider

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import HomeScreen from "../screens/Home";
 import PokedexScreen from "../screens/Pokedex";
 import PokemonScreen from "../screens/Pokemon";
@@ -11,13 +13,15 @@ const { Navigator, Screen } = createStackNavigator();
 
 const Stack: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Navigator headerMode="none">
-        <Screen name="Home" component={HomeScreen} />
-        <Screen name="Pokedex" component={PokedexScreen} />
-        <Screen name="Pokemon" component={PokemonScreen} />
-      </Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Navigator headerMode="none">
+          <Screen name="Home" component={HomeScreen} />
+          <Screen name="Pokedex" component={PokedexScreen} />
+          <Screen name="Pokemon" component={PokemonScreen} />
+        </Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
