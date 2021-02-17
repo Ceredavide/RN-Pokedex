f358dfa9e9b3bf0df8149108cd314fbcd568647d
Davide <ceredavide@live.it>
2021-02-17 15:25:45 +0100

    importato loadResourcesAsync in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
-import Stack from "./src/navigation/index"
-
-import loadResourcesAsync from "./src/services/loadResourcesAsync";
+import Stack from "./src/navigation/index";
 
 const App: React.FC = () => {
-  
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   if (isLoading) {
@@ -22,5 +21,19 @@ const App: React.FC = () => {
 
   return <Stack />;
 };
+
+async function loadResourcesAsync() {
+  try {
+    await Asset.loadAsync([require("./assets/images/loadingPokemon.gif")]);
+    await Font.loadAsync({
+      "Avenir-Book": require("./assets/fonts/Avenir-Book.otf"),
+      "Avenir-Heavy": require("./assets/fonts/Avenir-Heavy.ttf"),
+      "Avenir-Medium": require("./assets/fonts/Avenir-Medium.otf"),
+      "Avenir-Roman": require("./assets/fonts/Avenir-Roman.otf"),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default App;
