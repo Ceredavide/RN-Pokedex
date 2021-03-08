f5105cf896734a4120bd7c151d80ce94c300c7ea
Davide <ceredavide@live.it>
2021-03-08 14:12:26 +0100

    Create fonts.ts

---
+++ src/constants/fonts.ts
@@ -0,0 +1,12 @@
+const fonts: typeFont ={
+    AvenirHeavy: "Avenir-Heavy",
+    AvenirRoman: "Avenir-Roman",
+    AvenirMedium: "Avenir-Medium",
+    AvenirBook: "Avenir-Book"
+}
+
+type typeFont = {
+    [key: string]: string  
+  }
+
+export default fonts
