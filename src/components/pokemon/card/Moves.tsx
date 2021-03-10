91fa7221ff9847a819f0ade6be9be242ece56ffe
Davide <ceredavide@live.it>
2021-03-10 23:40:30 +0100

    aggiunto stile a test e a suo container

--- src/components/pokemon/card/Moves.tsx
+++ src/components/pokemon/card/Moves.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 import { Move } from "../../../models/Pokemon";
 
@@ -11,10 +12,32 @@ const Moves: React.FC<Props> = ({ moves }) => {
   return (
     <View>
       {moves.map((item, index) => {
-        return <Text key={index}>{item.move.name}</Text>;
+
+        const moveName: string = item.move.name.replaceAll('-', ' ');
+
+        return (
+          <View key={index} style={styles.type}>
+            <Text style={styles.text}>
+              {moveName}
+            </Text>
+          </View>
+        );
       })}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  type: {
+    padding: hp("2%"),
+    borderBottomWidth: 1,
+    borderBottomColor: "#d5d8dc"
+  },
+  text: {
+    fontSize: hp("1.7%"),
+    textTransform: 'capitalize',
+    fontFamily: "Avenir-Medium"
+  },
+});
 
 export default Moves;
