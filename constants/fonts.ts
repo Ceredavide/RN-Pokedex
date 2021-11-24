482caa50f4e4978676e8f14e18d64879f5ce787b
Davide <ceredavide@live.it>
2021-11-24 16:32:55 +0100

    correto nome

---
+++ constants/fonts.ts
@@ -0,0 +1,12 @@
+const fonts: Font ={
+    AvenirHeavy: "Avenir-Heavy",
+    AvenirRoman: "Avenir-Roman",
+    AvenirMedium: "Avenir-Medium",
+    AvenirBook: "Avenir-Book"
+}
+
+type Font = {
+    [key: string]: string  
+  }
+
+export default fonts
