1efe0ba14b8c3cc6915bb40b8a134fdeb992c38f
Davide <ceredavide@live.it>
2022-05-07 18:28:39 +0200

    modificato nome variabili e usato relative imports

--- hooks/useFetchPokemon.ts
+++ hooks/useFetchPokemon.ts
@@ -1,33 +1,34 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
-import PokemonInterface from "../models/Pokemon";
+import IPokemon, { Type } from "@interfaces/Pokemon";
 
-import handleError from "../services/handleError";
+import handleError from "@utils/handleError";
 
 const useFetchPokemon = (url: string) => {
   const [isLoading, setIsloading] = useState<boolean>(true);
-  const [error, setError] = useState<Object | null>(null);
-  const [pokemon, setPokemon] = useState<PokemonInterface | null>(null);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [pokemon, setPokemon] = useState<IPokemon>();
 
   useEffect(() => {
     const fetchPokemon = async () => {
       setIsloading(true);
       try {
-        const response1: any = await axios.get(url);
-        const response2 : any = await axios.get(
-          response1.data.varieties[0].pokemon.url
+        const pokemonSpeciesQuery: AxiosResponse = await axios.get(url);
+
+        const pokemonQuery: AxiosResponse = await axios.get(
+          pokemonSpeciesQuery.data.varieties[0].pokemon.url
         );
 
         // metto in ordine i tipi per i pokemon
-        response2.data.types.sort((a: any, b: any) => {
+        pokemonQuery.data.types.sort((a: Type, b: Type) => {
           return a.slot - b.slot;
         });
 
         setError(null);
 
-        setPokemon({ ...response1.data, ...response2.data });
-      } catch (error: any) {
+        setPokemon({ ...pokemonQuery.data, ...pokemonSpeciesQuery.data });
+      } catch (error: AxiosError | any) {
         setError(error);
         handleError(error);
       } finally {
